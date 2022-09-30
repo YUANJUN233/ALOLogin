06d55e8cacd4fa727d628e8129ca8a855c3ebec2
YUANJUN233 <1497657581@qq.com>
2022-09-30 22:37:24 +0800

    helloworld

---
+++ src/yuanjun/alol/ALOL.java
@@ -0,0 +1,10 @@
+package yuanjun.alol;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ALOL extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Start Plugin");
+    }
+}
